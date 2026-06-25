4981d1e3fc899f584ab1900e6ae2a5bf89805c81
SwaroopShindolkar <shindolkarswaroop@gmail.com>
2026-06-26 00:45:55 +0530

    PreFabs

--- Assets/Scripts/FlyAtPlayer.cs
+++ Assets/Scripts/FlyAtPlayer.cs
@@ -3,16 +3,31 @@ using UnityEngine;
 public class FlyAtPlayer : MonoBehaviour
 {
     [SerializeField] Transform player;
+    [SerializeField] float flySpeed = 1.0f;
     Vector3 playerPosition;
    
     void Start()
     {
         playerPosition = player.transform.position;
+
     }
 
     
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, playerPosition, 1f);
+        FlyOver();
+        DestroyWhenReached();
+    }
+    void FlyOver()
+    {
+        transform.position = 
+        Vector3.MoveTowards(transform.position, playerPosition, Time.deltaTime*flySpeed);
+    }
+    void DestroyWhenReached()
+    {
+        if (transform.position == playerPosition)
+        {
+            Destroy(gameObject);
+        }
     }
 }
